b248982b4ced00b9506519987da1b052ff855a46
winston <cryptoluffa@gmail.com>
2024-10-27 02:33:12 +0800

    feat: add auth

--- _cms.ts
+++ _cms.ts
@@ -1,3 +1,54 @@
-import cms from "blog/_cms.ts";
+import lumeCMS from "cms/mod.ts";
+import { Octokit } from "npm:octokit";
+import GitHub from "lume/cms/storage/github.ts";
+
+const username = Deno.env.get("USERNAME")!;
+const password = Deno.env.get("PASSWORD")!;
+
+const cms = lumeCMS({
+  auth: {
+    method: "basic",
+    users: {
+      [username]: password,
+    },
+  },
+});
+
+// Register GitHub storage
+cms.storage(
+  "src",
+  new GitHub({
+    client: new Octokit({ auth: Deno.env.get("GITHUB_TOKEN") }),
+    owner: "str-bin",
+    repo: "strbin.com",
+  })
+);
+
+// Configure an upload folder
+cms.upload("uploads", "gh:uploads");
+
+// Configure a collection
+cms.collection("posts", "gh", [
+  "title: text",
+  {
+    name: "summary",
+    type: "textarea",
+    attributes: {
+      required: true,
+    },
+  },
+  {
+    name: "image",
+    type: "file",
+    uploads: "uploads",
+    attributes: {
+      accept: "image/*",
+    },
+  },
+  "tags: list",
+  "draft: checkbox",
+  "show_toc: checkbox",
+  "content: markdown",
+]);
 
 export default cms;
